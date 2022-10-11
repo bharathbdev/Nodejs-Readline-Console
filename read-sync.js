67da095c2f8e68ba50aecc2053a6012020f135bc
Bharath B <bharath.b@intimetec.com>
2022-10-11 07:10:35 +0530

    reading input from cmd

---
+++ read-sync.js
@@ -0,0 +1,12 @@
+
+// Importing the module
+const readline = require("readline-sync");
+
+console.log("give small number")
+// Enter the number
+let a = Number(readline.question());
+let number = [];
+for (let i = 0; i < a; ++i) {
+    number.push(Number(readline.question()));
+}
+console.log(number);
